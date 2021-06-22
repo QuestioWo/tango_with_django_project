a1ad840c752e6976f2343c3e1f46fc5e7d039106
Jim Carty <cartyjim1@gmail.com>
2021-06-22 10:03:37 +0100

    chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -16,25 +16,25 @@ def populate():
 	
 	python_pages = [
 		{'title': 'Official Python Tutorial',
-		'url':'http://docs.python.org/3/tutorial/'},
+		'url':'http://docs.python.org/3/tutorial/', 'views':10},
 		{'title':'How to Think like a Computer Scientist',
-		'url':'http://www.greenteapress.com/thinkpython/'},
+		'url':'http://www.greenteapress.com/thinkpython/', 'views':20},
 		{'title':'Learn Python in 10 Minutes',
-		'url':'http://www.korokithakis.net/tutorials/python/'} ]
+		'url':'http://www.korokithakis.net/tutorials/python/', 'views':30} ]
 	
 	django_pages = [
 		{'title':'Official Django Tutorial',
-		'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+		'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views':40},
 		{'title':'Django Rocks',
-		'url':'http://www.djangorocks.com/'},
+		'url':'http://www.djangorocks.com/', 'views':50},
 		{'title':'How to Tango with Django',
-		'url':'http://www.tangowithdjango.com/'} ]
+		'url':'http://www.tangowithdjango.com/', 'views':60} ]
 
 	other_pages = [
 		{'title':'Bottle',
-		'url':'http://bottlepy.org/docs/dev/'},
+		'url':'http://bottlepy.org/docs/dev/', 'views':70},
 		{'title':'Flask',
-		'url':'http://flask.pocoo.org'} ]
+		'url':'http://flask.pocoo.org', 'views':80} ]
 
 	cats = {'Python': {'pages' : python_pages, 'views': 128, 'likes': 64},
                    'Django': {'pages' : django_pages, 'views': 64, 'likes': 32},
@@ -48,7 +48,7 @@ def populate():
 	for cat, cat_data in cats.items():
 		c = add_cat(cat, cat_data['views'], cat_data['likes'])
 		for p in cat_data['pages']:
-			add_page(c, p['title'], p['url'])
+			add_page(c, p['title'], p['url'], p['views'])
 
 	# Print out the categories we have added.
 	for c in Category.objects.all():
@@ -63,7 +63,9 @@ def add_page(cat, title, url, views=0):
 	return p
 
 def add_cat(name, views, likes):
-	c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+	c = Category.objects.get_or_create(name=name)[0]
+	c.views = views
+	c.likes = likes
 	c.save()
 	return c
 
